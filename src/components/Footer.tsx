9f0697b3fc5c03ad7dd4d27c8f2e938b9e58dd0d
lemkhayeryassine2002 <lemkhayeryassine2002@gmail.com>
2025-07-22 15:15:35 +0100

    another commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
             <div className="container">
                 <div className="grid sm:flex sm:justify-between">
                     <div className="copyright pt-10 text-sm">
-                        <span>All Right Reserved &copy; 2024</span>
+                        <span>All Right Reserved &copy; 2025</span>
                     </div>
                     <Sidebar2 />
                     {/* <div id="contact-form" className="scroll-mt-24 bg-spgray border rounded w-[520px] mt-10 px-20 py-10">
